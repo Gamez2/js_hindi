83aca9207f83daf474623c069d7cd0bdfeda27ce
Gamez2 <151774618+Gamez2@users.noreply.github.com>
2024-01-23 10:30:36 +0000

    conversion

---
+++ 01_basic/#2.js
@@ -0,0 +1,15 @@
+//type conversion 
+
+let score = null;
+console.log(typeof score);
+
+//NUmber should convert be converted
+let convertToNumber= Number(score);
+console.log(convertToNumber);
+
+//"33" => 33
+//"33asd"=>Nan
+// true=> 1 ,,,,,,,false => 0
+//null=> object
+//undefined => Nan
+
