03278863f175f096035e1fcfa69c7eee48a57f13
Gamez2 <151774618+Gamez2@users.noreply.github.com>
2024-01-24 10:45:47 +0000

    conversion  & operation

--- 01_basic/#2.js
+++ 01_basic/#2.js
@@ -1,11 +1,12 @@
 //type conversion 
 
 let score = null;
-console.log(typeof score);
+// console.log(typeof score);
+
 
 //NUmber should convert be converted
 let convertToNumber= Number(score);
-console.log(convertToNumber);
+// console.log(convertToNumber);
 
 //"33" => 33
 //"33asd"=>Nan
@@ -13,3 +14,17 @@ console.log(convertToNumber);
 //null=> object
 //undefined => Nan
 
+//operation
+
+let a =console.log("2"+2);
+let b =console.log("2"+"2");
+let c =console.log(2+"2");
+let d =console.log("2"+2+2);
+
+
+
+
+console.log( typeof a);
+console.log( typeof b);
+console.log( typeof c);
+console.log( typeof d);
