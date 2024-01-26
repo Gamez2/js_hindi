e97ce71c9cf0cd86ebac2864180470da6ee09f24
Gamez2 <151774618+Gamez2@users.noreply.github.com>
2024-01-26 13:53:04 +0000

    date and time

--- 01_basic/#5.js
+++ 01_basic/#5.js
@@ -1,26 +1,25 @@
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
-console.log(hello);
+//date and time
+
+let myDate = new Date();
+// console.log(myDate.toString());
+//.todateString
+//console.log(myDate.toString()); karo or 
+//.to string hai uske or jo function hai usse b hi try kr na hai
+
+let createDate = new Date(2023,11,4);
+// console.log(createDate.toDateString());
+
+//0 =jan // 11 = dec
+
+
+// console.log(myDate);
+
+// Explore date in mdn
+
+
+//****************Time**********************
+
+let myTimestamp =  Date.now();
+console.log(myTimestamp);
+ 
+console.log(createDate.getTime());
