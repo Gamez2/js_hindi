1d5fecba324745c2143d4c9789923e047d6d22c7
Gamez2 <151774618+Gamez2@users.noreply.github.com>
2024-03-06 14:06:06 +0000

    pro

--- 02_basic/prototype.js
+++ 02_basic/prototype.js
@@ -30,3 +30,29 @@ createUSer.prototype.printme = function(){
 }
 
 chai.printme();
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// prototype
+
+let myName = "dhruv    ";
+// now creta a own function in object beacuse
+
+// String ==>object 
+// function==>Object
+// array also a oject
+
+
+function  setName(username){
+    this.username =username;
+}
+
+function cretaeUserId (username , email , password){
+    setName.call(this,username);
+    this.email =email;
+    this.password =password;
+}
+
+const name1 = new cretaeUserId("chai","abcd@gmail.com","#pass_123");
+console.log(name1);
