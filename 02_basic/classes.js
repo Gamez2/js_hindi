68c65ab5dbc3de3417c2ce8b5d8735ac763e8f86
Gamez2 <151774618+Gamez2@users.noreply.github.com>
2024-03-07 09:17:42 +0000

    get $ set

--- 02_basic/classes.js
+++ 02_basic/classes.js
@@ -5,10 +5,20 @@ class user{
         this.password =password;
     }
 
+
+    // get wont work without set
+    get password(){
+        return this._password.toUpperCase();
+    }
+
+    set password(value){
+        this._password =value;
+    }
+
     setEncryptPass(password){
         return `${password}o;abf;odf`;
     }
 }
 let clint = new user("chai" , "heY@gmail.com" ,"dasfnb32");
 
-console.log(clint.setEncryptPass("hey threere f"));
+console.log(clint);
